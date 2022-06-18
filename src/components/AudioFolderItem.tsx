7212073a16b053bd518ef9e362a818b0ec9836d0
Kiwon Kim <kiwon@elicer.com>
2022-06-18 15:10:40 +0900

    feat: add style to audio folder item component

--- src/components/AudioFolderItem.tsx
+++ src/components/AudioFolderItem.tsx
@@ -5,8 +5,10 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  View,
   ViewStyle,
 } from 'react-native';
+import { useState } from 'react';
 
 export interface FolderHavingAudioFiles {
   id: number;
@@ -17,22 +19,63 @@ export interface FolderHavingAudioFiles {
 const AudioFolderItem: FC<
   FolderHavingAudioFiles & { style?: StyleProp<ViewStyle> }
 > = ({ id, name, files, style }) => {
+  const [open, setOpen] = useState(false);
+
+  const handlePress = () => {
+    setOpen((open) => !open);
+  };
+
   return (
-    <TouchableOpacity style={[styles.container, style]} activeOpacity={0.5}>
-      <Text>{name}</Text>
-    </TouchableOpacity>
+    <View style={[styles.container, style]}>
+      <TouchableOpacity
+        onPress={handlePress}
+        activeOpacity={0.7}
+        style={[styles.summaryContainer, { borderBottomWidth: open ? 1 : 0 }]}
+      >
+        <Text>{name}</Text>
+      </TouchableOpacity>
+      {open ? (
+        <View style={styles.detailsContainer}>
+          {files.map((file, index) => (
+            <TouchableOpacity
+              key={file.id}
+              activeOpacity={0.5}
+              style={[
+                styles.fileContainer,
+                { borderBottomWidth: index === files.length - 1 ? 0 : 1 },
+              ]}
+            >
+              <Text>{file.filename}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      ) : null}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: 'gainsboro',
+    borderColor: '#ededed',
+    overflow: 'hidden',
+  },
+  summaryContainer: {
     paddingVertical: 10,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#ededed',
+    backgroundColor: '#c6e7ff',
+  },
+  detailsContainer: {
+    backgroundColor: '#ffffff',
+  },
+  fileContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderColor: '#ededed',
   },
 });
 
