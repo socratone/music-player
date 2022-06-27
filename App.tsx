93c3150b6c2e0bee85f2aff59cf5e319ae7c6b66
Kiwon Kim <kiwon@elicer.com>
2022-06-28 01:03:08 +0900

    feat: add navigation bar background color

--- App.tsx
+++ App.tsx
@@ -3,8 +3,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AudioProvider from './src/contexts/audio';
 import color from './src/constants/color';
 import Navigation from './src/Navigation';
+import { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync(color.background);
+  }, []);
+
   return (
     <AudioProvider>
       <SafeAreaProvider style={styles.container}>
